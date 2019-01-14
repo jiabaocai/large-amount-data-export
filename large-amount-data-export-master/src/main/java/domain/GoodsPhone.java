bafebc6bbe9800b581d0ec9ec6298d5ffceeaefd
蔡家宝 <cjb0331@163.com>
2019-01-14 10:02:57 +0800

    ·

---
+++ large-amount-data-export-master/src/main/java/domain/GoodsPhone.java
@@ -0,0 +1,105 @@
+package domain;
+
+import java.io.Serializable;
+
+public class GoodsPhone implements Serializable {
+    private Integer id;
+
+    private Integer category_id;
+
+    private String band;
+
+    private String model;
+
+    private String color;
+
+    private String memory;
+
+    private String storage;
+
+    private String size;
+
+    private String unique_code;
+
+    private Integer order_id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Integer category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getUnique_code() {
+        return unique_code;
+    }
+
+    public void setUnique_code(String unique_code) {
+        this.unique_code = unique_code;
+    }
+
+    public Integer getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(Integer order_id) {
+        this.order_id = order_id;
+    }
+}
